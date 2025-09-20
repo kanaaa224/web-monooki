fca18382650ccd55037bec051468cad5faa5efa5
Kanato Shimabukuro <kanato.shimabukuro@gmail.com>
2025-09-20 16:26:08 +0900

    app-base-1

--- app-base-1/src/main.js
+++ app-base-1/src/main.js
@@ -1,261 +1,192 @@
-const { createApp, ref, onMounted, nextTick } = Vue;
-const { createVuetify, useTheme } = Vuetify;
+const { createApp, ref, onMounted, nextTick, reactive } = Vue;
+const { createVuetify, useTheme, useDisplay } = Vuetify;
 
-const vuetify = createVuetify({
-    theme: {
-        defaultTheme: window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light',
-        themes: {
-            light: {
-                dark: false,
-                colors: {
-                    background: '#fff',
-                    surface:    '#fff',
-                    primary:    '#2196f3',
-                    secondary:  '#444',
-                    error:      '#c23131'
-                }
-            },
-            dark: {
-                dark: true,
-                colors: {
-                    background: '#222',
-                    surface:    '#292929',
-                    primary:    '#2196f3',
-                    secondary:  '#eee',
-                    error:      '#c23131'
-                }
-            }
-        }
-    }
-});
+(async () => {
+    let api_default_endpoint_url = API_ENDPOINTS_URLS[0];
 
-const app = createApp({
-    setup() {
-        const logging = (d = '') => {
-            console.log(`[ ${(new Date()).toISOString()} ] ${d}`);
-        };
+    const callAPI = async (endpoint = api_default_endpoint_url, queries = {}, requestBody = null) => {
+        const url = new URL(endpoint);
 
-        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        for(const [ key, value ] of Object.entries(queries)) url.searchParams.set(key, value);
 
-        const callAPI = async (uri = '', queries = '', requestBody = null, endpoint = API_ENDPOINTS[0]) => {
-            uri = `${endpoint}${uri}`;
+        let request = { method: 'GET' };
 
-            if(queries) uri += /\?/.test(uri) ? `&${queries}` : `?${queries}`;
+        if(requestBody) request = { method: 'POST', body: JSON.stringify(requestBody) };
 
-            let request = { method: 'GET' };
+        const response = await fetch(url.toString(), request);
+        const body     = await response.json();
 
-            if(requestBody) request = { method: 'POST', body: JSON.stringify(requestBody) };
+        if(!response.ok) throw new Error(`api-bad-status: ${response.status}`);
 
-            const response = await fetch(uri, request);
-            const data     = await response.json();
-
-            if(!response.ok) throw new Error(`api-bad-status: ${response.status}`);
-
-            return data;
-        };
-
-        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-        const snackbar_visible = ref(false);
-        const snackbar_message = ref('');
-        const snackbar_color   = ref('');
-        const snackbar_time    = ref(5000);
-
-        const snackbar = (message = null, color = null, time = null) => {
-            if(!snackbar_visible.value) {
-                snackbar_message.value = message ?? snackbar_message.value;
-                snackbar_color.value   = color   ?? snackbar_color.value;
-                snackbar_time.value    = time    ?? snackbar_time.value;
-                snackbar_visible.value = true;
-            }
-        };
-
-        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-        const dialog_loading_visible = ref(false);
-        const dialog_loading_title   = ref('');
-        const dialog_loading_icon    = ref('');
-
-        const dialog_loading = (title = null, icon = null) => {
-            if(!dialog_loading_visible.value) {
-                dialog_loading_title.value   = title ?? dialog_loading_title.value;
-                dialog_loading_icon.value    = icon  ?? dialog_loading_icon.value;
-                dialog_loading_visible.value = true;
-            }
-        };
-
-        const dialog_settings_visible = ref(false);
-
-        const dialog_settings = () => {
-            dialog_settings_visible.value = true;
-        };
-
-        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-        const container_visible = ref(false);
-        const developer         = ref({});
-
-        const onLoad = async () => {
-            developer.value = await callAPI();
-
-            ((l) => (l.href = developer.value.avatar_url, document.head.appendChild(l)))(document.querySelector("link[rel='icon']")             || Object.assign(document.createElement("link"), { rel: "icon" }));
-            ((l) => (l.href = developer.value.avatar_url, document.head.appendChild(l)))(document.querySelector("link[rel='apple-touch-icon']") || Object.assign(document.createElement("link"), { rel: "apple-touch-icon" }));
-
-            container_visible.value = true;
-        };
-
-        const theme = useTheme();
-
-        onMounted(() => {
-            window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
-                theme.global.name.value = e.matches ? 'dark' : 'light';
-            });
-
-            window.addEventListener('load', onLoad);
-        });
-
-        const APP_VERSION = 'v1.0';
-        const APP_NAME    = 'アプリ';
-
-        document.title = APP_NAME;
-
-        return {
-            theme,
-
-            APP_VERSION,
-            APP_NAME,
-
-            snackbar_visible,
-            snackbar_message,
-            snackbar_color,
-            snackbar_time,
-            snackbar,
-
-            dialog_loading_visible,
-            dialog_loading_title,
-            dialog_loading_icon,
-            dialog_loading,
-            dialog_settings_visible,
-            dialog_settings,
-
-            developer,
-
-            container_visible
-        }
-    },
+        return body;
+    };
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    template: `
-        <v-app>
-            <v-snackbar
-                v-model="snackbar_visible"
-                :timeout="snackbar_time"
-                :color="snackbar_color"
-            >{{ snackbar_message }}</v-snackbar>
-            <v-dialog
-                v-model="dialog_loading_visible"
-                max-width="320"
-                persistent
-            >
-                <v-list
-                    class="py-2"
-                    color="primary"
-                    elevation="12"
-                    rounded="lg"
+    const app = createApp({
+        setup() {
+            const theme   = useTheme();
+            const display = useDisplay();
+
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+            const developer = ref({});
+
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+            const dialog_settings_visible = ref(false);
+
+            const dialog_settings = () => {
+                if(!dialog_settings_visible.value) {
+                    dialog_settings_visible.value = true;
+
+                    return;
+                }
+            };
+
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+            const container_visible = ref(false);
+
+            onMounted(async () => {
+                document.title = APP_NAME;
+
+                window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
+                    theme.global.name.value = e.matches ? 'dark' : 'light';
+                });
+
+                try {
+                    developer.value = await callAPI();
+                } catch(e) {
+                    console.error(e);
+                }
+
+                ((l) => (l.href = developer.value.avatar_url, document.head.appendChild(l)))(document.querySelector("link[rel='icon']")             || Object.assign(document.createElement("link"), { rel: "icon" }));
+                ((l) => (l.href = developer.value.avatar_url, document.head.appendChild(l)))(document.querySelector("link[rel='apple-touch-icon']") || Object.assign(document.createElement("link"), { rel: "apple-touch-icon" }));
+
+                container_visible.value = true;
+            });
+
+            return {
+                api,
+
+                theme,
+                display,
+
+                developer,
+
+                dialog_settings_visible,
+                dialog_settings,
+
+                container_visible
+            }
+        },
+        template: `
+            <v-app style="padding-top: env(safe-area-inset-top); padding-bottom: env(safe-area-inset-bottom);">
+                <v-dialog
+                    v-model="dialog_settings_visible"
+                    transition="dialog-bottom-transition"
+                    fullscreen
                 >
-                    <v-list-item
-                        :prepend-icon="dialog_loading_icon"
-                        :title="dialog_loading_title"
-                    >
-                        <template v-slot:prepend>
-                            <div class="pe-4">
-                                <v-icon color="primary" size="x-large"></v-icon>
-                            </div>
-                        </template>
-                        <template v-slot:append>
-                            <v-progress-circular
-                                indeterminate="disable-shrink"
-                                size="16"
-                                width="2"
-                            ></v-progress-circular>
-                        </template>
-                    </v-list-item>
-                </v-list>
-            </v-dialog>
-            <v-dialog
-                v-model="dialog_settings_visible"
-                transition="dialog-bottom-transition"
-                fullscreen
-            >
-                <v-card>
-                    <v-toolbar>
-                        <v-toolbar-items>
+                    <v-card style="padding-top: env(safe-area-inset-top);">
+                        <v-toolbar>
+                            <v-toolbar-title>設定</v-toolbar-title>
+                            <v-toolbar-items>
+                                <v-btn
+                                    icon="mdi-close"
+                                    @click="dialog_settings_visible = false"
+                                />
+                            </v-toolbar-items>
+                        </v-toolbar>
+                        <v-list lines="two">
+                            <v-list-subheader title="アプリケーション" />
+                            <v-list-item
+                                title="バージョン"
+                                subtitle="v1.0"
+                            />
+                            <v-divider />
+                            <v-list-item
+                                class="text-center"
+                                subtitle="© 2025 kanaaa224. All rights reserved."
+                                href="https://kanaaa224.github.io/"
+                                target="_blank"
+                                rel="noopener"
+                            />
+                        </v-list>
+                    </v-card>
+                </v-dialog>
+                <v-main>
+                    <v-fade-transition mode="out-in">
+                        <v-container v-if="container_visible"></v-container>
+                    </v-fade-transition>
+                    <v-fade-transition mode="out-in">
+                        <div
+                            v-if="container_visible"
+                            class="d-flex flex-column"
+                            style="position: fixed; top: 2rem; right: 2rem; z-index: 999;"
+                        >
                             <v-btn
-                                icon="mdi-close"
-                                @click="dialog_settings_visible = false"
-                            ></v-btn>
-                        </v-toolbar-items>
-                        <v-toolbar-title>設定</v-toolbar-title>
-                    </v-toolbar>
-                    <v-list lines="two">
-                        <v-list-subheader>アプリケーション</v-list-subheader>
-                        <v-list-item
-                            title="バージョン"
-                            :subtitle="APP_VERSION"
-                        ></v-list-item>
-                        <v-divider></v-divider>
-                        <v-list-item
-                            class="text-center"
-                            subtitle="© 2025 kanaaa224. All rights reserved."
-                            link
+                                icon
+                                variant="plain"
+                                @click="dialog_settings()"
+                            ><v-icon icon="mdi-cog" /></v-btn>
+                            <v-btn
+                                icon
+                                variant="plain"
+                                @click="theme.global.name.value = theme.global.name.value === 'dark' ? 'light' : 'dark'"
+                            ><v-icon :icon="theme.global.name.value === 'dark' ? 'mdi-weather-night' : 'mdi-white-balance-sunny'" /></v-btn>
+                        </div>
+                    </v-fade-transition>
+                </v-main>
+                <v-footer class="justify-center pa-2" style="margin-bottom: env(safe-area-inset-bottom); opacity: 0.25; background-color: transparent;" app>
+                    <span class="text-body-2">
+                        © 2025 <a
+                            style="color: inherit;"
                             href="https://kanaaa224.github.io/"
                             target="_blank"
                             rel="noopener"
-                        ></v-list-item>
-                    </v-list>
-                </v-card>
-            </v-dialog>
-            <v-main>
-                <transition name="fade">
-                    <v-container v-if="container_visible"></v-container>
-                </transition>
-                <transition name="fade">
-                    <div
-                        class="d-flex flex-column"
-                        style="position: fixed; top: 2rem; right: 2rem; z-index: 999;"
-                    >
-                        <v-btn
-                            v-if="container_visible"
-                            icon
-                            variant="plain"
-                            @click="dialog_settings()"
-                        ><v-icon>mdi-cog</v-icon></v-btn>
-                        <v-btn
-                            v-if="container_visible"
-                            icon
-                            variant="plain"
-                            @click="theme.global.name.value = theme.global.name.value === 'dark' ? 'light' : 'dark'"
-                        ><v-icon>{{ theme.global.name.value === 'dark' ? 'mdi-weather-night' : 'mdi-white-balance-sunny' }}</v-icon></v-btn>
-                    </div>
-                </transition>
-            </v-main>
-            <v-footer
-                app
-                class="justify-center pa-2"
-                style="opacity: 0.25; background-color: transparent;"
-            >
-                <span class="text-body-2">
-                    © 2025 <a
-                        style="color: inherit;"
-                        href="https://kanaaa224.github.io/"
-                        target="_blank"
-                        rel="noopener"
-                    >kanaaa224</a>. All rights reserved.
-                </span>
-            </v-footer>
-        </v-app>
-    `
-});
+                        >kanaaa224</a>. All rights reserved.
+                    </span>
+                </v-footer>
+            </v-app>
+        `
+    });
 
-app.use(vuetify).mount('#app');
+    const vuetify = createVuetify({
+        theme: {
+            defaultTheme: window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light',
+            themes: {
+                light: {
+                    dark: false,
+                    colors: {
+                        background: '#fff',
+                        surface:    '#fff',
+                        primary:    '#2196f3',
+                        secondary:  '#444',
+                        error:      '#c23131'
+                    }
+                },
+                dark: {
+                    dark: true,
+                    colors: {
+                        background: '#222',
+                        surface:    '#292929',
+                        primary:    '#2196f3',
+                        secondary:  '#eee',
+                        error:      '#c23131'
+                    }
+                }
+            }
+        }
+    });
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    try {
+        await api.connect();
+    } catch(e) {
+        console.error(e);
+    }
+
+    app.use(vuetify).mount('#app');
+})();
